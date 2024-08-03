b380868b9ed801525237cb54a8eed23710e317e6
Adrian Tarver <21149460+Th3Tr1ckst3r@users.noreply.github.com>
2024-08-03 15:13:52 -0600

    Add files via upload
    Signed-off-by: Adrian Tarver <21149460+Th3Tr1ckst3r@users.noreply.github.com>

---
+++ lmcache.py
@@ -0,0 +1,72 @@
+import dns.resolver
+import lmdb
+import datetime
+import json
+import re
+import itertools
+
+
+class LMCache:
+    def __init__(self, path='lm_cache.lmdb', ttl=10):
+        self.env = lmdb.open(path, map_size=15 * 1024 ** 3)  # Set map_size to 15 GB
+        self.ttl = ttl
+        self.nameservers = itertools.cycle(['1.0.0.1', '1.1.1.1', '8.8.8.8', '208.67.222.222', '208.67.220.220'])
+
+    def get(self, domain):
+        with self.env.begin() as txn:
+            value = txn.get(domain.encode('utf-8'))
+            if value:
+                data = json.loads(value.decode('utf-8'))
+                timestamp = datetime.datetime.fromisoformat(data['timestamp'])
+                if (datetime.datetime.now(datetime.timezone.utc) - timestamp).total_seconds() < self.ttl:
+                    return data['ip']
+                else:
+                    self.delete(domain)  # Cache expired, delete entry
+        return None
+
+    def set(self, domain, ip):
+        with self.env.begin(write=True) as txn:
+            data = {
+                'ip': ip,
+                'timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat()
+            }
+            txn.put(domain.encode('utf-8'), json.dumps(data).encode('utf-8'))
+
+    def delete(self, domain):
+        with self.env.begin(write=True) as txn:
+            txn.delete(domain.encode('utf-8'))
+
+    def resolve_domain(self, domain):
+        # Sanitize the domain
+        domain = re.sub(r'[\'"\\]', '', domain)
+        
+        resolver = dns.resolver.Resolver()
+        
+        # Select the next nameserver in the round-robin sequence
+        resolver.nameservers = [next(self.nameservers)]
+        
+        cached_ip = self.get(domain)
+        if cached_ip:
+            return cached_ip
+        
+        try:
+            # Resolve 'A' (IPv4) records
+            answers_v4 = resolver.resolve(domain, 'A')
+            for ipval in answers_v4:
+                ip = str(ipval)
+                self.set(domain, ip)
+                return ip
+            
+            # If no IPv4 records found, attempt to resolve 'AAAA' (IPv6) records
+            answers_v6 = resolver.resolve(domain, 'AAAA')
+            for ipval in answers_v6:
+                ip = str(ipval)
+                self.set(domain, ip)
+                return ip
+            
+        except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.Timeout):
+            pass
+        except Exception:
+            pass
+        
+        return None
